6f92a1920a311a4bae03a649fae15af25d22bff9
RahidAmin <rahidamin19@gmail.com>
2023-11-19 01:05:13 +0600

    Shipping charge added

--- Simple-ecommerce-website/src/Components/Product/Product.jsx
+++ Simple-ecommerce-website/src/Components/Product/Product.jsx
@@ -12,7 +12,7 @@ const Product = (props) => {
    const handelAddToCart=props.handelAddToCart;
     return (
         <div className='product'>
-             <img src={img} alt="" />
+             <img src={img} alt=""/>
             <div className='product-info'>
            
             <h6 className='product-name'>{name}</h6> 
